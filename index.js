282837125202eb4dc8f08519539fe1257a467f3b
kim tran <icycleitaly@gmail.com>
2018-03-20 17:48:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function prependKitten(name){
+  newArr = [name, ...kittens];
+  return newArr;
+}
+
+function removeLastKitten(){
+  newArr = kittens.slice(0,kittens.length-1);
+  return kittens;
+}
+
+function removeFirstKitten(){
+ newArr = kittens.slice(1,kittens.length);
+}
+
